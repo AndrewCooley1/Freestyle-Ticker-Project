71ef45e8597c48cf14777f99771f765dffdd9b57
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-08-06 22:26:55 -0400

    Attempting updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
-from flask import Flask
+#from flask import Flask
 
-from web_app.routes.stock_routes import stock_routes
+#from web_app.routes.stock_routes import stock_routes
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(stock_routes)
+#def create_app():
+    #app = Flask(__name__)
+    #app.register_blueprint(stock_routes)
    
-    return app
+    #return app
 
-if __name__ == "__main__":
-    my_app = create_app()
-    my_app.run(debug=True)
+#if __name__ == "__main__":
+    #my_app = create_app()
+    #my_app.run(debug=True)
--- web_app/routes/stock_routes.py
+++ web_app/routes/stock_routes.py
@@ -1,14 +1,15 @@
-# web_app/routes/weather_routes.py
+# web_app/routes/stock_routes.py
 
 from flask import Blueprint, request, render_template
 from app.stock import lookup_ticker
+
 stock_routes = Blueprint("stock_routes", __name__)
 
 
 @stock_routes.route("/")
 def home():
-    print("Home")
-    return render_template("stocks.html")
+    print("Home...")
+    return render_template("stock.html")
 
 @stock_routes.route("/handle_request", methods=["POST"])
 def post_handler():
@@ -17,6 +18,8 @@ def post_handler():
 
     print("FORM DATA:", dict(request.form))
     request_data = dict(request.form)
+
+# do we need a request.args function?
 
     symbol = request_data.get("ticker")
     
